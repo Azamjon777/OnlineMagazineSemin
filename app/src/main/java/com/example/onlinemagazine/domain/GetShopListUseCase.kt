5b0e66109d120473d054972a165642ca86ff7e20
Azamjon Ostonakulov <azamjonostonakulov@gmail.com>
2022-10-25 23:47:03 +0500

    Domain слой

---
+++ app/src/main/java/com/example/onlinemagazine/domain/GetShopListUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.onlinemagazine.domain
+
+import com.example.onlinemagazine.domain.model.ShopItemModel
+
+class GetShopListUseCase {
+    fun getShopList(): List<ShopItemModel> {
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/example/onlinemagazine/domain/model/ShopItemModel.kt
@@ -0,0 +1,8 @@
+package com.example.onlinemagazine.domain.model
+
+data class ShopItemModel(
+    val id: Int,
+    val name: String,
+    val count: Int,
+    val enabled: Boolean
+)
